9ad422a8ada71fa0cb2602885de40c97ac56a2c1
White-Li-Ai <1324218065@qq.com>
2025-12-29 22:52:42 +0800

    docs:代码逐行注释--未完待续

--- hangzhou_jobs_analysis/job_crawler.py
+++ hangzhou_jobs_analysis/job_crawler.py
@@ -114,7 +114,7 @@ class BOSSCrawler:
         #返回包含10个模拟岗位数据的列表
         return page_data
     def save_to_csv(self):
-        """保存数据到CSV文件
+        """保存数据到CSV文件中
         功能：将当前爬取到岗位数据保存为csv格式的文件
         """
         if self.jobs_data:
